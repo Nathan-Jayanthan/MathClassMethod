47553f30ab76ce5a41cc3c46bc5ef4582425534f
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-04 14:22:41 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       //always gives absolute
+        System.out.println(Math.abs(-33.4));
+        //rounds up
+        System.out.println(Math.ceil(7.4));
+        //rounds down
+        System.out.println(Math.floor(7.4));
+        //gives max of two numbers
+        System.out.println(Math.max(7,6));
+        //gives min of two numbers
+        System.out.println(Math.min(7,6));
+        //first number to the power od second number
+        System.out.println(Math.pow(7,6));
+        //first number to the power od second number
+        System.out.println(Math.sqrt(9));
+
+    }
+}
